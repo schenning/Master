20547fb93f042e3406fe2b84731b8aadb82e9860
Henning <henning.schei@gmail.com>
2017-07-05 14:32:54 +0200

    Added comments to make the code more understandable

--- rmusic.m
+++ rmusic.m
@@ -6,29 +6,33 @@ function w = rmusic(x,n,m,fs)
 %      x  ->  the data vector
 %      n  ->  the model order
 %      m  ->  the order of the covariance matrix 
+%      fs ->  sampling rate (optionally, default fs=1)
 %      w  <-  the frequency estimates
 
-% variable check
+
+% input check
     narginchk(3,4)
     if nargin == 3
         fs = 1;
     end
+    assert (numel(x) > n, 'ERROR, the number of samples in the signal are less than the subspace dimension');
 
-    
+
+
+% Compute autocovariance matrix of the inout signal        
     R = compute_autocovariance(x,m);
-% singular value decomposition 
+
+% Singular value decomposition 
     [U,S,V] = svd(R);
     G = U(:,n+1:end);
     P = G * conj(G)';
-
-% Forward backward approach?
 
 % Calculate sum of each diagonal in P
     [r ,c]=size(P);
     idx=bsxfun(@plus,(r:-1:1)',0:c-1);
     Q=flipud(accumarray(idx(:),P(:)));
 
-% Find the roots inside the unit circle and imag-val != 0    
+% Find the roots inside the unit circle with imag value != 0 (DC-component)   
     rts = roots(Q);
     rts = rts(abs(rts) < 1);
     rts = rts(imag(rts) ~=0);
@@ -37,14 +41,14 @@ function w = rmusic(x,n,m,fs)
     idx_sort= argsort(dfc);
     component_roots = rts(idx_sort(1:n));
     
-
     ang = -angle(component_roots);
     w = fs*ang;
-%%
+%% end rmusic
+
 function R = compute_autocovariance(x,M)
     % Henning Schei
     % Function to compute the corvarinace matrix of a signal. 
-    
+    % The function is similar to built in function corrmtx(), but with different normalization facotr    
     
     % x --> array of input signal of size N
     % M --> int, opitional. Size of signal block
@@ -52,7 +56,7 @@ function R = compute_autocovariance(x,M)
     
     N = length(x); 
     
-    x_vect = x(:); % Force x to be a column vector
+    x_vect = x(:); % Force x_vect to be a column vector of x
         
     % Initial covariance matrix
     yn = flipud(x_vect(1:M));
